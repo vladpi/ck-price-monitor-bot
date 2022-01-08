b9a5c298f9ddc2b9e8e7700adafb17bacbc6beaa
Vlad Pastushenko <vladp@ro.ru>
2022-01-08 18:26:00 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import sys
+
+import httpx
+from bs4 import BeautifulSoup
+from deta import app
+from telegram.bot import Bot
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+MY_USER_ID = os.environ.get('MY_USER_ID')
+ITEMS = [
+    ('https://www.charleskeith.co.th/th-en/CK2-80270728_TAUPE_S-TH.html', 2590.0),
+    ('https://www.charleskeith.co.th/th-en/CK2-20671280_IVORY_S-TH.html', 2790.0),
+    ('https://www.charleskeith.co.th/th-en/CK2-40781624_BLACK_L-TH.html', 2990.0),
+]
+
+
+def parse_item_price(url: str) -> float:
+    response = httpx.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.content, 'lxml')
+    price_tag = soup.find('meta', {'itemprop': 'price'})
+
+    if price_tag is not None:
+        return float(price_tag.attrs.get('content'))
+
+    else:
+        raise Exception(f'Цена не найдена на странице: {url}')
+
+
+@app.lib.cron()
+def main(_):
+    bot = Bot(BOT_TOKEN)
+
+    try:
+        for item_url, item_base_price in ITEMS:
+            price = parse_item_price(item_url)
+            if price != item_base_price:
+                bot.send_message(
+                    MY_USER_ID,
+                    f'Новая цена на товар!\n{item_base_price:.2f} -> {price:.2f}\n{item_url}',
+                )
+    except Exception as ex:
+        exc_type, _, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+
+        bot.send_message(
+            MY_USER_ID,
+            f'Что-то пошло не так.\nОшибка {exc_type} {fname}:{exc_tb.tb_lineno}\n{ex}',
+        )
+
+        raise ex
