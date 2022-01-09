f3c53d09c887d08dbf0d0626a676933d33cf799b
Vlad Pastushenko <vladp@ro.ru>
2022-01-09 19:46:11 +0300

    Added storing items in db

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import os
 import sys
+from typing import TYPE_CHECKING, Any
 
 import httpx
 from bs4 import BeautifulSoup
+from deta import Deta
 from dotenv import load_dotenv
 from telegram.bot import Bot
+
+if TYPE_CHECKING:
+    from deta import Base
 
 load_dotenv()
 
 IS_DETA = os.environ.get('DETA_RUNTIME') == 'true'
+
+DETA_PROJECT_KEY = os.environ.get('DETA_PROJECT_KEY')
+DETA_DB_NAME = os.environ.get('DETA_DB_NAME', 'ck-items-local')
+
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 MY_USER_ID = os.environ.get('MY_USER_ID')
 
-ITEMS = [
-    ('https://www.charleskeith.co.th/th-en/CK2-80270728_TAUPE_S-TH.html', 2590.0),
-    ('https://www.charleskeith.co.th/th-en/CK2-20671280_IVORY_S-TH.html', 2790.0),
-    ('https://www.charleskeith.co.th/th-en/CK2-40781624_BLACK_L-TH.html', 2990.0),
-]
+EXAMPLE_ITEM_KEY = 'EXAMPLE_ITEM'
+EXAMPLE_ITEM = {
+    'key': EXAMPLE_ITEM_KEY,
+    'url': 'https://www.charleskeith.co.th/th-en/CK2-80270728_TAUPE_S-TH.html',
+    'base_price': 2590.0,
+    'user_id': MY_USER_ID,
+}
 
 
 def parse_item_price(url: str) -> float:
@@ -33,17 +44,31 @@ def parse_item_price(url: str) -> float:
         raise Exception(f'Цена не найдена на странице: {url}')
 
 
-def main():
+def init_deta_db(deta: Deta) -> 'Base':
+    db = deta.Base(DETA_DB_NAME)
+
+    if db.fetch().count == 0:
+        db.put(data=EXAMPLE_ITEM_KEY)
+
+    return db
+
+
+def main() -> None:
     bot = Bot(BOT_TOKEN)
+    db = init_deta_db(Deta(DETA_PROJECT_KEY))
+    items = db.fetch().items
 
     try:
-        for item_url, item_base_price in ITEMS:
-            price = parse_item_price(item_url)
-            if price != item_base_price:
+        for item in items:
+            url = item['url']
+            base_price = item['base_price']
+            price = parse_item_price(url)
+            if price != item['base_price']:
                 bot.send_message(
                     MY_USER_ID,
-                    f'Новая цена на товар!\n{item_base_price:.2f} -> {price:.2f}\n{item_url}',
+                    f'Новая цена на товар!\n{base_price:.2f} -> {price:.2f}\n{url}',
                 )
+
     except Exception as ex:
         exc_type, _, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -60,7 +85,7 @@ if IS_DETA:
     from deta import app
 
     @app.lib.cron()
-    def check_prices_task(_):
+    def check_prices_task(event: Any) -> None:
         main()
 
 elif __name__ == '__main__':
